a2eaa4241889b749146b05b3dea95cd0d98470a6
Ahmed Badry <ra6514201@gmail.com>
2024-08-08 13:31:48 +0300

    "Added CSS styles for login form, updated HTML structure, and implemented JavaScript validation and API call for login functionality."

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function emailValidation() {
+    let email = document.getElementById('email').value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+
+    return emailRegex.test(email);
+}
+
+function passwordValidation() {
+    let password = document.getElementById('password').value;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g;
+
+    return passwordRegex.test(password);
+}
+
+document.getElementById('submit-btn').addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    if (emailValidation()) {
+        await sendData();
+    } else {
+        console.log('Invalid email or password');
+    }
+});
+
+async function sendData() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let url = 'https://reqres.in/api/login';
+    let config = {
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        let response = await fetch(url, config);
+        
+        if (response.ok) {
+            let data = await response.json();
+            localStorage.setItem('token', data.token);
+            console.log(data);
+            console.log('Login successfully');
+        } else {
+            console.log('Error:', response.status);
+        }
+    } catch (error) {
+        console.log('Network error:', error);
+    }
+}
+
